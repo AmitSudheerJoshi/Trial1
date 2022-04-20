95f7b6efcbefdc324bf5e22c7054172e45a7b635
AmitSudheerJoshi <103834175+AmitSudheerJoshi@users.noreply.github.com>
2022-04-20 19:50:42 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,1 +1,6 @@
 #Learning python is fun. Python requires no prior coding experience. It is a powerful Object Oriented programming language.
+# I will now try to write code in here and see if i can save/download it to my pc.
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+%pylab
